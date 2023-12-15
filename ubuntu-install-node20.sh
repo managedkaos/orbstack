b4216c922b3e73b74cde595f2cf3b281a98af72c
Michael <managedkaos@users.noreply.github.com>
2023-12-15 13:21:44 -0800

    Add script to install node20 on ubuntu

---
+++ ubuntu-install-node20.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --batch --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" > /etc/apt/sources.list.d/nodesource.list
+
+apt-get update
+apt install -y nodejs
+npm install --global yarn
+
+for i in node npm npx yarn;
+do
+	echo "$(which ${i}) $(${i} --version)"
+done
