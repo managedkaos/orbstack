d064f4d7315638764d90734e8cd8558f7035d392
Michael <managedkaos@users.noreply.github.com>
2024-01-02 23:01:24 -0800

    update docker and python scripts

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -6,8 +6,8 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable | \
   tee /etc/apt/sources.list.d/docker.list
 
 apt-get update
@@ -20,3 +20,4 @@ apt-get install -y \
   docker-compose-plugin
 
 docker run hello-world
+usermod -aG docker "Michael.Jenkins"
--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade -y
-apt-get install -y ca-certificates curl gnupg wget git
+apt-get install -y \
+  build-essential \
+  ca-certificates \
+  curl \
+  git \
+  gnupg \
+  python3-dev \
+  python3-pip \
+  python3-venv \
+  wget
 sudo install -m 0755 -d /etc/apt/keyrings
-
