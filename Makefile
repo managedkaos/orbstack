51ce5858aa82627e8d116a59ec0baded486df701
Michael <managedkaos@users.noreply.github.com>
2025-01-22 19:36:57 -0800

    tweak the rocky 9 recipe

--- Makefile
+++ Makefile
@@ -12,19 +12,24 @@ list:
 
 server:
 	orb create --arch $(ARCH) $(OS):$(VERSION) $(NAME)
+	@echo "ssh $(NAME)@orb"
 
 ubuntu:
 	orb create --user-data ubuntu/init.yml --arch $(ARCH) $(OS):$(VERSION) $(NAME)
+	@echo "ssh $(NAME)@orb"
 
 rocky:
 	orb create --arch $(ARCH) rocky:9 $(NAME)
 	orb -m $(NAME) sudo ./rocky/init.sh
+	@echo "ssh $(NAME)@orb"
 
 jupyter-hub:
 	orb create --user-data ubuntu/jupyter-hub.yml --arch $(ARCH) $(OS):$(VERSION) $(NAME)
+	@echo "ssh $(NAME)@orb"
 
 apache:
 	orb create --user-data ubuntu/apache.yml --arch $(ARCH) $(OS):$(VERSION) $(NAME)
+	@echo "ssh $(NAME)@orb"
 
 $(TARGETS): server
 	@echo "### Installing $(@)"
--- rocky/init.sh
+++ rocky/init.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+
+# Set ulimit so core dumps are generated
 ulimit -c unlimited
-yum install -y varnish nginx git wget zip unzip vim
+
+# Install required packages
+yum install -y \
+    epel-release \
+    varnish \
+    nginx \
+    git \
+    wget \
+    zip \
+    unzip \
+    vim \
+    python3-pip \
+    awscli
+
+# Change the default port of Nginx to 8080
 sed -i 's/80/8080/' /etc/nginx/nginx.conf
+
+# Start and enable Varnish and Nginx
 systemctl enable --now varnish nginx
+
+# Take a break :D
 sleep 1
-curl -s http://localhost:6081 | grep Nginx
+
+# Check if Varnish is working
+curl -s http://localhost:6081 | \
+    grep Nginx && \
+    echo "Varnish is working" || echo "Varnish is not working"
